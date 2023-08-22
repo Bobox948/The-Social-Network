b03ab7c7e364747d142c96a816dcca2b83850caf
Boris <boris.pietrzak@gmail.com>
2023-08-22 22:11:26 +0200

    test

--- app.js
+++ app.js
@@ -95,8 +95,11 @@ app.use(function(req, res, next) {
 
 
 
-app.listen(3000, () => console.log("app listening on port 3000!"));
-
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+  });
+  
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
